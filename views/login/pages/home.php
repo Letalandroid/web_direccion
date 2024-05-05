fa423ebf28ffbb5e59891cf878284ec4e9558ba9
Letalandroid <carlossoncra@gmail.com>
2024-05-05 15:55:39 -0500

    add: ➕ New page login

--- views/login/pages/home.php
+++ views/login/pages/home.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/views/apoderado/assets/img/logo_transparent.png" type="image/x-icon" />
-    <title>I.E.P Los Clavelitos de SJT - Piura | Login</title>
+    <title>LOGIN</title>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="/CSS/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+   
 </head>
 <body>
-    <h1>login</h1>
+    <form class="fr1">
+        <div class="img">
+        <img src="/img/icon.png" alt="">
+        </div>
+        <h4 ><b>I.E.P</h4>
+        <h4 class ="d1"><b>LOS CLAVELITOS DE SAN JUDAS TADEO</b></h4>
+
+        <label for="username"><b>Usuario</b></label>
+        <input type="text" placeholder="" id="username">
+
+        <label for="password"><b>Contraseña</b></label>
+        <input type="password" placeholder="" id="password">
+
+        <button class="btn-grow-ellipse">Iniciar sesión</button>
+        <p><a href="">Olvide mi contraseña</a></p>
+    </form>
+    <div class="contacto">
+        
+    </div>
 </body>
 </html>
