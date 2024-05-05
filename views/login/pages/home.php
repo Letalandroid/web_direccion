6d9a1d3db3b2850daa462ddea461cc11fc740295
Letalandroid <carlossoncra@gmail.com>
2024-05-05 16:16:21 -0500

    fix: Route to img/*, css/* & add: placeholder on inputs

--- views/login/pages/home.php
+++ views/login/pages/home.php
@@ -1,32 +1,40 @@
+<?php
+
+$path = __DIR__;
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <title>LOGIN</title>
- 
+    <title>I.E.P Los Clavelitos de SJT - Piura | Login</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="stylesheet" href="/CSS/login.css">
+    <link rel="stylesheet" href="/views/login/css/login.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
-   
+    <link rel="shortcut icon" href="/views/apoderado/assets/img/logo_transparent.png" type="image/x-icon">
 </head>
+
 <body>
-    <form class="fr1">
+    <form action="#" method="post" class="fr1">
         <div class="img">
-        <img src="/img/icon.png" alt="">
+            <img src="/views/login/assets/img/icon.png" alt="">
         </div>
-        <h4 ><b>I.E.P</h4>
-        <h4 class ="d1"><b>LOS CLAVELITOS DE SAN JUDAS TADEO</b></h4>
+        <h4><b>I.E.P</h4>
+        <h4 class="d1"><b>Los clavelitos de San Judas Tadeo</b></h4>
 
         <label for="username"><b>Usuario</b></label>
-        <input type="text" placeholder="" id="username">
+        <input type="text" placeholder="0987654321" id="username">
 
         <label for="password"><b>Contraseña</b></label>
-        <input type="password" placeholder="" id="password">
+        <input type="password" placeholder="*******************" id="password">
 
         <button class="btn-grow-ellipse">Iniciar sesión</button>
         <p><a href="">Olvide mi contraseña</a></p>
     </form>
     <div class="contacto">
-        
+
     </div>
 </body>
+
 </html>
