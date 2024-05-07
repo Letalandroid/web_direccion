efbb2baa6dc006ecd63e237b4ce205e64d328364
Letalandroid <carlossoncra@gmail.com>
2024-05-07 16:52:40 -0500

    Log with user and password to DB

---
+++ controllers/Usuarios.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Letalandroid\controllers;
+
+require_once __DIR__ . '/../model/db.php';
+
+use Letalandroid\model\Database;
+use Exception;
+use PDO;
+
+class Usuarios
+{
+
+    static function getAll()
+    {
+        try {
+            $db = new Database();
+
+            $query = $db->connect()->prepare('select * from usuarios;');
+            $query->execute();
+
+            $results = $query->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
+        } catch (Exception $e) {
+            http_response_code(500);
+            return array('error' => true, 'message' => 'Error en el servidor: ' . $e);
+        }
+    }
+}
--- views/apoderado/pages/asistencias.php
+++ views/apoderado/pages/asistencias.php
@@ -1,3 +1,13 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: /');
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
