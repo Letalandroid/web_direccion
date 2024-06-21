cac5963d95b1a90a2c6a11d2c3755a5be5c21ca2
jeremyOlaya <jeremyolayavasquez01@gmail.com>
2024-06-21 00:45:15 -0600

    yata conducta mi rey

--- views/apoderado/js/conductas.js
+++ views/apoderado/js/conductas.js
@@ -1,9 +1,9 @@
 const btnActivate = (id) => {
     const btnActivate = document.querySelector(`#btn_${id}`);
-    const container_activate = document.querySelector(`.n__${id}`);
-    const section_notes = document.querySelector(`.sec_${id}`);
+    const containerActivate = document.querySelector(`.n__${id}`);
+    const sectionNotes = document.querySelector(`.sec_${id}`);
 
-    container_activate.classList.toggle('activate');
+    containerActivate.classList.toggle('activate');
     btnActivate.classList.toggle('activate_btn');
-    section_notes.classList.toggle('sparcing');
+    sectionNotes.classList.toggle('sparcing');
 }
